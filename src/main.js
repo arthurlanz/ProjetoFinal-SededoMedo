62886b35f555ed01012efeed3f9172a56b037889
arthurlanzn <arthurlanznaster@gmail.com>
2025-11-30 04:46:47 -0300

    feat: Adição de visualizações de catálogo de filmes e séries com opções de filtragem.
    - Integração de ícones do Font Awesome ao aplicativo.
    
    - Adição de novas rotas para FilmsView e SeriesView com confirmação de idade.
    
    - Criação de um novo armazenamento para gerenciamento de dados de séries.
    
    - Implementação dos métodos discoverMovies e discoverSeries para buscar dados com parâmetros personalizados.
    
    - Desenvolvimento dos componentes FilmsView e SeriesView com carrosséis de destaque, filtros e estados de carregamento.
    
    - Melhoria da interface do usuário com design responsivo e indicadores de carregamento.
    #14, #17, #23

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import App from './App.vue'
 import VueLazyLoad from 'vue3-lazyload'
 import './assets/main.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas, fab, far)
+
 const app = createApp(App)
 const pinia = createPinia()
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(pinia)
 app.use(router)
